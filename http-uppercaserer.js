c4b2b02fa200c8e90b33a1ca273460ace4b411dc
Abhishek4298 <59391928+Abhishek4298@users.noreply.github.com>
2020-01-09 11:31:36 +0530

    update

---
+++ http-uppercaserer.js
@@ -0,0 +1,12 @@
+t http = require('http');
+const map = require('through2-map');
+
+let server = http.createServer(function (req,res) {
+	if(req.method === 'POST') {
+	  req.pipe(map(function (chunk) {
+	    res.write(chunk.toString().toUpperCase());
+	  }));
+	}	  
+});
+
+server.listen(+process.argv[2]);
